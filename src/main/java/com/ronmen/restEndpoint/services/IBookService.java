8388ea6245729e29020379bbee55ddf32b1b8b4b
RolandNemeth <Roland_Nemeth@epam.com>
2018-08-26 10:08:02 +0200

    service interfaces have been added

---
+++ src/main/java/com/ronmen/restEndpoint/services/IBookService.java
@@ -0,0 +1,37 @@
+package com.ronmen.restEndpoint.services;
+
+import com.ronmen.restEndpoint.persistence.entities.Book;
+import java.util.Collection;
+import org.springframework.http.ResponseEntity;
+
+public interface IBookService {
+
+  /**
+   * Reads all books related to given userId
+   *
+   * @param userId
+   * @return a {@link Collection} of books
+   */
+    Collection<Book> readBooks (String userId);
+
+  /**
+   * Add a book to the given user
+   *
+   * @param userId
+   * @param book
+   * @return a {@link ResponseEntity} with a status code of 201 (CREATED) and
+   * a header (Location) that the client can consult to learn
+   * how the newly created record is referencable.
+   */
+  ResponseEntity<?> addBook (String userId, Book book);
+
+  /**
+   * Read a book with the given userId and bookId
+   *
+   * @param userId
+   * @param bookId
+   * @return a {@Book}
+   */
+  Book readBook(String userId, Long bookId);
+
+}
---
+++ src/main/java/com/ronmen/restEndpoint/services/IUserService.java
@@ -0,0 +1,16 @@
+package com.ronmen.restEndpoint.services;
+
+import com.ronmen.restEndpoint.persistence.entities.User;
+import org.springframework.http.HttpStatus;
+
+public interface IUserService {
+  /**
+   * Validate whether a {@link User} exists with the given userId.
+   * The implemented solution has to handle situation where user is not found.
+   * It's highly recommended to sign negative result a specific {@link RuntimeException}
+   * which also sets the HTTP status code as well (to e.g.: {@link HttpStatus#NOT_FOUND})
+   *
+   * @param userId
+   */
+  void validateUser (String userId);
+}
