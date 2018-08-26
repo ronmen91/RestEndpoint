a4b18cb4f68ba247d49fdb476d2ff3003ffdadb6
RolandNemeth <Roland_Nemeth@epam.com>
2018-08-26 10:08:55 +0200

    not found errors related exception have been added which also set the HTTPSTATUS as well

---
+++ src/main/java/com/ronmen/restEndpoint/services/exceptions/BookNotFoundException.java
@@ -0,0 +1,11 @@
+package com.ronmen.restEndpoint.services.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class BookNotFoundException extends RuntimeException{
+  BookNotFoundException (Long bookId) {
+    super("Book is not found with bookid: " + bookId);
+  }
+}
